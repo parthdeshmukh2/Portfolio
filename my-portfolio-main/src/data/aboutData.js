321f849a09f03c6b98f4be0055b331b494950ecd
parthdeshmukh2 <parthdeshmukh99@gmail.com>
2022-06-10 17:34:50 +0530

    about data

--- my-portfolio-main/src/data/aboutData.js
+++ my-portfolio-main/src/data/aboutData.js
@@ -3,6 +3,6 @@ export const aboutData = {
   description1:
     "Full-Stack Web Developer",
   description2:
-    "Right now I have finished my Full Stack Web Development cousre from Masai School, Bengaluru. I love to learn and explore new areas. I am proficient in MERN stack and eager to learn and ready to contribute to the development of an organization.",
+    "I'm a full-stack web developer passionate about finding quick and efficient solutions to problems. Using my skills  I have built 2 projects. As a problem solver who loves facing challenges in my work, I'm excited to be at the deployment phase of my career as a web developer. I'm passionate and ambitious about my work and love to be on a team that questions possibilities.so, let's connect & start building!",
   image: 2,
 };

3e6d17f917ea9e49f22fdf72e0d1375bdea6b469
parthdeshmukh2 <parthdeshmukh99@gmail.com>
2022-06-16 16:17:19 +0530

    updated

--- my-portfolio-main/src/data/aboutData.js
+++ my-portfolio-main/src/data/aboutData.js
@@ -3,6 +3,6 @@ export const aboutData = {
   description1:
     "Full-Stack Web Developer",
   description2:
-    "I'm a full-stack web developer passionate about finding quick and efficient solutions to problems. Using my skills  I have built few projects. As a problem solver who loves facing challenges in my work, I'm excited to be at the deployment phase of my career as a web developer. I'm passionate and ambitious about my work and love to be on a team that questions possibilities.dkjdhvsd ",
+    "I'm a full-stack web developer passionate about finding quick and efficient solutions to problems. Using my skills  I have built few projects. As a problem solver who loves facing challenges in my work, I'm excited to be at the deployment phase of my career as a web developer. I'm passionate and ambitious about my work and love to be on a team that questions possibilities. ",
   image: 2,
 };
